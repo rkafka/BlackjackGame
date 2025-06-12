55f4cf17ea297c0f01f6df25c22cadb0e962d409
RKafka <rkafka14@gmail.com>
2025-06-12 17:09:46 -0500

    Testing folder added

---
+++ Tests/ModelsTests.cs
@@ -0,0 +1,15 @@
+namespace BlackjackGame.Models;
+
+class ModelsTests
+{
+    void Execute(string[] args)
+    {
+        test_Hand_Deck();
+    }
+
+    void test_Hand_Deck()
+    {
+        Hand hand = new Hand(false);
+    }
+}
+
