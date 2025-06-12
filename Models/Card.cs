467af9675d0051ec22a02de39ed734d2403b26df
RKafka <rkafka14@gmail.com>
2025-06-12 16:01:11 -0500

    Card: getValue(), Constructor, getCardName()

--- Models/Card.cs
+++ Models/Card.cs
@@ -3,27 +3,88 @@ using System.Reflection.Metadata;
 
 namespace BlackjackGame.Models;
 
-public class Card(string suit, int rank)
+public class Card
 {
     // Static fields (shared across all Card objects)
     public static readonly Dictionary<string, string> suitDict = new Dictionary<string, string> {
-        { "♠", "Spades" }, { "♠", "Clubs" },
-        { "♥", "Hearts" }, { "♦", "Diamonds" },
+        { "♠", "Spade" }, { "♠", "Club" },
+        { "♥", "Heart" }, { "♦", "Diamond" },
     };
+    public const int numberOfRanks = 13;
+    public static readonly string[] rankToStr = [
+        "Joker",    // 0
+        "Ace",      // 1
+        "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+        "Jack",     // 11
+        "Queen",    // 12
+        "King"      // 13
+     ];
 
 
     // Instance fields (unique to each instance of a Card object)
-    public string Suit { get; } = suit;
-    public int Rank { get; } = rank;
+    public string Suit { get; }
+    public int Rank { get; }
+    public int Value;
+
+    public Card(string suit, int rank)
+    {
+        Suit = suit;
+        Rank = rank;
+        if (rank < 1 || rank > 13)
+            throw new ArgumentOutOfRangeException(nameof(rank), "Rank must be in range [1,13]");
+        Value = getValue(rank);   
+    }
 
 
-    // public string getCardName()
-    // {
-    //     string cardName = $"{suitDict.}";
+    /**
+     */
+    public static int getValue(int rank, int currentHandValue = 0)
+    {
+        return rank switch
+        {
+            // JOKER
+            0 => throw new ArgumentOutOfRangeException(nameof(rank), "JOKER of RANK 0 is not supported"),
+            // ACE
+            1 => ((currentHandValue > 10) ? 1 : 11),// assumes you want highest value unless 
+            2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10 => rank,
+            // JACK
+            11 or 12 or 13 => 10,
+            _ => throw new ArgumentOutOfRangeException(nameof(rank), "Ranks greater than 13 are not supported."),
+        };
+        // OLD VERSION
+        // switch (rank)
+        // {
+        //     case 0: // JOKER
+        //         throw new ArgumentOutOfRangeException("getValue() -- JOKER of RANK 0 is not supported");
+        //     case 1: // ACE
+        //         // assumes you want highest value unless 
+        //         return ((currentHandValue > 10) ? 1 : 11);
+        //     case 2:
+        //     case 3:
+        //     case 4:
+        //     case 5:
+        //     case 6:
+        //     case 7:
+        //     case 8:
+        //     case 9:
+        //     case 10:
+        //         return rank;
+        //     case 11: // JACK
+        //     case 12: // QUEEN
+        //     case 13: // KING
+        //         return 10;
+        //     default:
+        //         throw new ArgumentOutOfRangeException("getValue() -- ranks greater than 13 are not supported.");
+        // }
+    }
 
-
-
-    //         return;
-    // }
+    /**
+     */
+    public string getCardName()
+    {
+        if (Rank == 0)
+            throw new ArgumentOutOfRangeException("Rank of Zero was assigned to a card, this is not allowed.");
+        return $"{rankToStr[Rank]} of {suitDict[Suit]}s";
+    }
 
 }
