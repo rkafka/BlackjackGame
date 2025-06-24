a0155f033c2cd9183501c064126418ffcc66698e
RKafka <rkafka14@gmail.com>
2025-06-24 17:01:08 -0500

    Added safety to set the colors to defaults (black, white), cleaned comments

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@ using BlackjackGame.Utils;
 using BlackjackGame.Core;
 
 
+// Ensure clean execution
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Clear();
 
+// new
+IGameUI ui;
+if (args.Length > 0 && args[0] == "ascii") { ui = new UI_ASCII(); }
+else { ui = new UI_TextBased(); }
+
+BJGame game = new BJGame(ui);
+game.Run();
 
 
 // GAMEPLAY SEQUENCE
@@ -34,100 +45,7 @@ using BlackjackGame.Core;
             - Dealer busts if over 21
 */
 
-// void Old()
-//     Startup.PrintTitle();
-//     if(args.Length > 0 && args[0] == "debug")
-//         ModelsTests.Execute(args);
 
-//     /// 1. Initial Deal
-//     ///     - Dealer gives 2 cards to each player (face up)
-//     ///     - Dealer gets 2 cards: one face up, one face down (the “hole” card)
-//     Deck deck = new Deck(doShuffle:true);
-//     deck.Print();
-
-//     User user = new();
-//     Dealer dealer = new();
-//     Game game = new(deck, user, dealer);
-
-
-//     // Console.ForegroundColor = ConsoleColor.White;
-//     int x, y;
-//     (x, y) = Console.GetCursorPosition();
-//     Startup.BootSequence();
-//     Console.Clear();
-
-//     bool gameOver = false;
-//     while (!gameOver)
-//     {
-//         // Initial Deal
-//         Startup.PrintTitle();
-
-//         game.SetBet();
-
-//         game.InitialDraw();
-
-
-//         game.PrintAllHandsAsText();
-//         // game.UI_Hands();
-//         // Blackjack Check
-//         int roundStatus = game.BlackjackCheck();
-//         bool roundOver = false;
-//         switch (roundStatus)
-//         {
-//             case 0:
-//                 // nobody wins, continue
-//                 if (game.PlayerActions() != 0)
-//                 {
-//                     // Dealer's turn
-//                     game.DealersTurn();
-//                 }
-//                 game.DecideWinner();
-//                 if (game._user._currentMoney <= 0)
-//                     gameOver = true;
-//                 else
-//                     game.ResetCards();
-//                 break;
-//             case 1:
-//                 // user wins! :) end round
-//                 roundOver = true;
-//                 game.resultWin();
-//                 game.ResetCards();
-//                 continue;
-//             case 2:
-//                 // dealer wins... :( end round
-//                 roundOver = true;
-//                 game.resultLose();
-//                 if (game._user._currentMoney <= 0)
-//                     gameOver = true;
-//                 else
-//                     game.ResetCards();
-//                 continue;
-//             case 3:
-//                 // BOTH got blackjack
-//                 // end round, no winner
-//                 roundOver = true;
-//                 game.resultTie();
-//                 game.ResetCards();
-//                 continue;
-//             default:
-//                 throw new ArgumentOutOfRangeException(nameof(roundStatus), "returned invalid value from BlackjackCheck(). Accepted values are 0, 1, 2, and 3");
-//         }
-
-//         // // Player Actions
-//         // int returnCode_PlayerActions = game.PlayerActions();
-
-
-//         // if (game._user._currentMoney <= 0)
-//         // {
-//         //     gameOver = true;
-//         // }
-//         // else //TO-DO: reset the hands (and deck?)
-//         // {
-//         //     game.ResetCards();
-//         // }
-//     }
-//     game.GameOver();
-// }
 // WIN CONDITIONS
 /* 
     Player busts    ------->    Player loses
@@ -137,22 +55,10 @@ using BlackjackGame.Core;
     Player == Dealer  ----->    Push (tie, no win or loss)
 */
 
+
 // PAYOUTS
 /*
     Regular win:    1:1 (bet $10, win $10)
     Blackjack:      3:2 (bet $10, win $15)
     Insurance:      2:1 (side bet if dealer shows an Ace — not recommended!)
 */
-
-// Ensure clean execution
-Console.BackgroundColor = ConsoleColor.Black;
-Console.ForegroundColor = ConsoleColor.White;
-Console.Clear();
-
-// new
-IGameUI ui;
-if (args.Length > 0 && args[0] == "ascii") { ui = new UI_ASCII(); }
-else { ui = new UI_TextBased(); }
-
-BJGame game = new BJGame(ui);
-game.Run();
